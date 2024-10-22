9316bf9aaa80a9ff6e226a4fd48e11812dd0fd75
Bal krishna Thapa Chhetri <118071778+BalThapa@users.noreply.github.com>
2024-10-22 14:18:17 +0300

    Post is now done posts are visible in home page

--- index.js
+++ index.js
@@ -9,19 +9,38 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+const blogPosts = []; //Array for dtoring blogs
+
 app.get("/", (req, res)=> {
-    res.render("index.ejs", {currentPath:req.path}); // passing the current path from your server to the 
-                                                        //EJS template instead of trying 
-                                                        //to access window.location.pathname directly.
+    res.render("index.ejs", {
+        currentPath:req.path,       // passing the current path from your server to the 
+                                        //EJS template instead of trying 
+                                             //to access window.location.pathname directly.
+        posts: blogPosts
+    });                                             
 });
+
 app.get("/create", (req, res)=> {
     res.render("create.ejs",{currentPath:req.path});
 });
 
 app.post("/submit", (req,res) => {
-    
+    const userName = req.body.userName;
+    const userEmail = req.body.userEmail;
+    const title = req.body.title;
+    const content = req.body.content;
+    const createdOn = req.body.createdOn;
+   
+    blogPosts.push ({
+        userName:userName,
+        userEmail:userEmail,
+        title:title,
+        content:content, 
+        createdOn:createdOn  
+    })
     res.render("index.ejs", {
-      
+        currentPath:req.path,
+        posts: blogPosts
     })
    
 })
